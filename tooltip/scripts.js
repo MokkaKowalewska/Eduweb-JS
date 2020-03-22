9f8fd5a7b81e4c1187be2f395957fbadf803005a
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-03-22 23:36:06 +0100

    tooltip completed

--- tooltip/scripts.js
+++ tooltip/scripts.js
@@ -1,12 +1,56 @@
 (function() {
 
-  function init(elems) {
+	let globalTooltip = null;
 
-    console.log(elems);
+    function assignEvents(elems, type, event) {
 
-  }
+        for(let i = 0; i < elems.length; i++) {
+            elems[i].addEventListener(type, event, false);
+        }
+
+	}
+
+	function createTooltip(text, options) {
+		let tooltip = document.createElement("div");
+		tooltip.className = "tooltip hidden";
+		tooltip.appendChild(document.createTextNode(text));
+		document.body.appendChild(tooltip);
+		tooltip.style.left = options.x + options.w / 2 - tooltip.offsetWidth / 2 + "px";
+		tooltip.style.top = options.y - tooltip.offsetHeight - 10 + "px";
+		tooltip.classList.remove("hidden");
+		globalTooltip = tooltip;
+	}
+
+	function showTooltip(e) {
+		let options = {
+			w: e.target.offsetWidth,
+			x: e.target.offsetLeft,
+			y: e.target.offsetTop
+		};
+
+		let text = e.target.getAttribute("title");
+		createTooltip(text, options);
+
+		e.target.removeAttribute("title");
 
 
-  window.t00ltip = init;
+
+
+	}
+
+	function removeTooltip(e) {
+		e.target.setAttribute("title", globalTooltip.textContent);
+		globalTooltip.parentNode.removeChild(globalTooltip);
+	}
+
+  	function init(elems) {
+
+   	 assignEvents(elems, "mouseenter", showTooltip);
+   	 assignEvents(elems, "mouseleave", removeTooltip);
+
+  	}
+
+
+  	window.t00ltip = init;
 
 })();
